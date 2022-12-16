447ca4c754d8a6dded0b1613c53e9878ca2e60e8
Clément Cadieux <cadieuxclement@gmail.com>
2022-12-16 11:39:15 -0500

    Pushed to database

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 from dotenv import load_dotenv
 import os
 import firebase_admin
-from firebase_admin import credentials
+from firebase_admin import firestore, credentials
 
 load_dotenv()
 
@@ -11,20 +11,31 @@ api_key = os.environ.get("API_KEY")
 db_key_path = os.environ.get("DB_KEY_PATH")
 
 # response is the array of movies
-response = requests.get(f"{api_link}/top_rated?api_key={api_key}&language=en-US&page=1").json()["results"]
 
 # Drop everything we don't need
 # Feed the array back with only needed info
-
 cred = credentials.Certificate(f"{db_key_path}")
-firebase_admin.initialize_app(cred)
+app = firebase_admin.initialize_app(cred)
+db = firestore.client()
 
-for movie in response:
-    del movie['adult']
-    del movie['backdrop_path']
-    del movie['original_title']
-    del movie['popularity']
-    del movie['video']
-    del movie['vote_count']
+page = 1
 
+while page <= 500:
+    response = requests.get(f"{api_link}/top_rated?api_key={api_key}&language=en-US&page={page}").json()["results"]
 
+    if page == 1:
+        page -= 1
+
+    page += 10
+
+    for movie in response:
+        del movie['adult']
+        del movie['backdrop_path']
+        del movie['original_title']
+        del movie['popularity']
+        del movie['video']
+        del movie['vote_count']
+
+        ref = db.collection(u"movies").document(str(movie['id']))
+
+        ref.set(movie)
